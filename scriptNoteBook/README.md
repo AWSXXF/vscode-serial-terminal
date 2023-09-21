34d4df278bfa71cffee3e0d26f10ff93abbd6d38
ydd <ydd_180@163.com>
2023-09-22 00:01:23 +0800

    [feat]  Preliminary separation of script notebook

---
+++ scriptNoteBook/README.md
@@ -0,0 +1,14 @@
+# Notebook with support for vscode terminal
+
+---
+
+## Tutorials
+
+1. Install this extension
+
+2. Create and open a file with the srcnb extension
+
+3. Add a code block and fill in the code
+
+4. Click the Run button to the left of the code block
+
--- serialTerminal/src/extension.ts
+++ serialTerminal/src/extension.ts
@@ -1,10 +1,8 @@
 import * as vscode from 'vscode';
 import { registerCommands } from './commands';
 import { registerContextCallback } from './contextManager';
-import { registerScriptNotebookSerializer } from './scriptNotebookSerializer';
 import { registerSerialPortView } from './serialPortView';
 import { registerLogView } from './logView';
-import { registerScriptNotebookController } from './scriptNotebookController';
 import { registerScriptView } from './scriptView';
 import { registerReadOnlyDocument } from './readOnlyDcoument';
 
@@ -16,8 +14,6 @@ export function activate(context: vscode.ExtensionContext) {
 	registerSerialPortView(context);
 	registerLogView(context);
 	registerScriptView(context);
-	registerScriptNotebookSerializer(context);
-	registerScriptNotebookController(context);
 	registerContextCallback(context);
 	registerReadOnlyDocument(context);
 }
